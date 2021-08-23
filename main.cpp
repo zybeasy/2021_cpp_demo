5d334c2ea745b19ec0a2e5a75c302e1db3cbb884
zybeasy <zybeasy@163.com>
2021-08-23 23:50:21 +0800

    2021年 8月23日 星期一 23时50分21秒 CST

--- main.cpp
+++ main.cpp
@@ -3,9 +3,30 @@
 #include <unistd.h>
 #include <sys/syscall.h>
 
-#include "test.h"
+#include "tree_test.h"
 
 using namespace std;
+
+class A {
+
+    public:
+    virtual void f() { cout << "A" << endl; }
+
+};
+
+
+class B: public A {
+    public:
+    virtual void f() { cout << "B" << endl; }
+
+};
+
+void func(A &a) { cout << &a << endl; a.f(); }
+
+void my_new_handler() {
+    cout << "FUCK NEW HANDLER ERROR" << endl;
+    exit(255);
+}
 
 int main(int, char**) {
 
@@ -19,4 +40,10 @@ int main(int, char**) {
 
     const char* FORMAT = "============";
     cout << FORMAT << " END " << FORMAT << endl;
+
+    cout << typeid(3).name()  << endl;
+    cout << typeid(3).hash_code() << endl;
+
+   void *data = (void*)malloc(0);
+   cout << data << endl;
 }
--- tree_test.cpp
+++ tree_test.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#include "test.h"
+#include "tree_test.h"
 
 using namespace std;
 
